55b69c272e803ac9f1d6ecab60c3ceafa4331eb7
wawra <wawra@05f62fea-98e3-1e44-aab5-ce008673666c>
2015-09-09 12:23:16 +0000

    git-svn-id: https://jamaika.kt.tu-cottbus.de:8443/svn/databases/uasr-data-coins/trunk@137 05f62fea-98e3-1e44-aab5-ce008673666c

--- LIVE/java/src/gui/gui.java
+++ LIVE/java/src/gui/gui.java
@@ -2,20 +2,38 @@ package gui;
 
 import java.awt.*;
 import java.awt.event.ComponentEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class gui extends Frame {
 
-	int recognizedCoin = 001;
+	int recognizedCoin = 4;
+	boolean sure = false;
 	int width;
 	int height;
+	String abs;
+	String rel = "";
+	String start = "Erkannte Münze:";
+	String notSure = "?";	
+	String yesSure = "!";
+	Image img;
 	
-	public gui(int width, int height){
+	public gui(int w, int h){
 		super("Münzerkennung");
 		addWindowListener(new WindowClosingAdapter(true));
 		setBackground(Color.black);
 
-		setSize(width,height);
-		setVisible(true);		
+		setSize(w,h);
+		setVisible(true);
+		
+		img = null;
+		abs = "\\LIVE\\java\\src\\img";
+		try {
+			rel = new File(".").getCanonicalPath();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("HOME Verzeichnis nicht gefunden.");
+		}
 	}
 	
 	public gui(){
@@ -26,36 +44,59 @@ public class gui extends Frame {
 		
 		setSize(600,480);
 		setVisible(true);
+		
+		img = null;
+		abs = "\\LIVE\\java\\src\\img";
+		try {
+			rel = new File(".").getCanonicalPath();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("HOME Verzeichnis nicht gefunden.");
+		}
+	}
+	
+	public void setRecognizedCoin(int coin){
+		recognizedCoin = coin;
+	}
+	
+	public void setSure (boolean s){
+		sure = s;
 	}
 	
 	public void paint(Graphics g){
 		width = getSize().width;
 		height = getSize().height;
-		
-		String start = "Erkannte Münze:";
+
 		Font topFont;
+		Font SureFont;
 		topFont = new Font("SansSerif",Font.PLAIN,36);
+		SureFont = new Font("Serif",Font.PLAIN,72);
 		g.setFont(topFont);
 		FontMetrics fm = g.getFontMetrics();
 		int startLength = fm.stringWidth(start);
 		g.setColor(Color.green);
 		g.drawString(start, (width-startLength)/2, 150);
 		
-		Image img = null;
 		switch(recognizedCoin){
-		case 001: img = getToolkit().getImage(getClass().getResource("bitmaps\\C001.jpg")); break;
-		case 002: img = getToolkit().getImage(getClass().getResource("bitmaps\\C002.jpg")); break;
-		case 005: img = getToolkit().getImage(getClass().getResource("bitmaps\\C005.jpg")); break;
-		case 010: img = getToolkit().getImage(getClass().getResource("bitmaps\\C010.jpg")); break;
-		case 020: img = getToolkit().getImage(getClass().getResource("bitmaps\\C020.jpg")); break;
-		case 050: img = getToolkit().getImage(getClass().getResource("bitmaps\\C050.jpg")); break;
-		case 100: img = getToolkit().getImage(getClass().getResource("bitmaps\\C100.jpg")); break;
-		case 200: img = getToolkit().getImage(getClass().getResource("bitmaps\\C200.jpg")); break;
-		default:  img = getToolkit().getImage(getClass().getResource("bitmaps\\C000.jpg")); break;
+		case 001: img = getToolkit().getImage(rel + abs + "\\C001.jpg"); break;
+		case 002: img = getToolkit().getImage(rel + abs + "\\C002.jpg"); break;
+		case 005: img = getToolkit().getImage(rel + abs + "\\C005.jpg"); break;
+		case 010: img = getToolkit().getImage(rel + abs + "\\C010.jpg"); break;
+		case 020: img = getToolkit().getImage(rel + abs + "\\C020.jpg"); break;
+		case 050: img = getToolkit().getImage(rel + abs + "\\C050.jpg"); break;
+		case 100: img = getToolkit().getImage(rel + abs + "\\C100.jpg"); break;
+		case 200: img = getToolkit().getImage(rel + abs + "\\C200.jpg"); break;
+		default:  img = getToolkit().getImage(rel + abs + "\\C000.png"); break;
 		}
 		g.drawImage(img, (width-100)/2, 200, this);
+		
+		g.setFont(SureFont);
+		if (sure){
+			g.drawString(yesSure,(width+150)/2, 275);
+		} else {
+			g.drawString(notSure,(width+150)/2, 275);
+		}
 	}
-	
 	public static void main(String[] args){
 		Frame window = new gui();
 	}
