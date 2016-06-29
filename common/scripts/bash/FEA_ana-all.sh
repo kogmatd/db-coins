8f55884a55dc834e8ab35dfa8f76d22314d398cc
wolff <wolff@05f62fea-98e3-1e44-aab5-ce008673666c>
2016-06-29 13:53:44 +0000

    o [fixed] batch processing scripts o ignored nohup.out files
    git-svn-id: https://jamaika.kt.tu-cottbus.de:8443/svn/databases/uasr-data-coins/trunk@236 05f62fea-98e3-1e44-aab5-ce008673666c

--- common/scripts/bash/FEA_ana-all.sh
+++ common/scripts/bash/FEA_ana-all.sh
@@ -4,5 +4,5 @@ DBHOME=$UASR_HOME-data/coins
 
 cd $DBHOME/Versuch1;   nohup FEA.xtp ana info/Versuch1.mcfg   2>&1 &
 cd $DBHOME/Versuch2;   nohup FEA.xtp ana info/Versuch2.mcfg   2>&1 &
-cd $DBHOME/Versuch2;   nohup FEA.xtp ana info/Versuch3.mcfg   2>&1 &
+cd $DBHOME/Versuch3;   nohup FEA.xtp ana info/Versuch3.mcfg   2>&1 &
 cd $DBHOME/Vorversuch; nohup FEA.xtp ana info/Vorversuch.mcfg 2>&1 &
--- common/scripts/bash/HMM_trn-all.sh
+++ common/scripts/bash/HMM_trn-all.sh
@@ -4,5 +4,5 @@ DBHOME=$UASR_HOME-data/coins
 
 cd $DBHOME/Versuch1;   nohup HMM.xtp trn info/Versuch1.mcfg   2>&1 &
 cd $DBHOME/Versuch2;   nohup HMM.xtp trn info/Versuch2.mcfg   2>&1 &
-cd $DBHOME/Versuch2;   nohup HMM.xtp trn info/Versuch3.mcfg   2>&1 &
+cd $DBHOME/Versuch3;   nohup HMM.xtp trn info/Versuch3.mcfg   2>&1 &
 cd $DBHOME/Vorversuch; nohup HMM.xtp trn info/Vorversuch.mcfg 2>&1 &
