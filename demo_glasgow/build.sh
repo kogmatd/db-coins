1f7b685d941a11141aa749ae2e9dac406dff5453
duckhfra <duckhfra@05f62fea-98e3-1e44-aab5-ce008673666c>
2017-10-19 08:50:52 +0000

    expand demo_glasgow to 5 coins
    git-svn-id: https://jamaika.kt.tu-cottbus.de:8443/svn/databases/uasr-data-coins/trunk@527 05f62fea-98e3-1e44-aab5-ce008673666c

--- demo_glasgow/build.sh
+++ demo_glasgow/build.sh
@@ -13,7 +13,7 @@ grep Correctness train.log
 REC_PACKDATA.xtp rec info/recpack.cfg
 REC_PACKDATA.xtp rec info/recpack.cfg -Pouttyp=F
 
-for I in c001 c010 c200; do
+for I in c001 c010 c200 c005 c050; do
 	echo $I
 	recfix -o dec model/recfix.cfg sig_ori/$I* 2>/dev/null | grep "res fst 0" | sort | uniq -c
 done
